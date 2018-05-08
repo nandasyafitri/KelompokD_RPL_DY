2bd148a78475f382490fd025b3ae0649da730e00
Budi Gunawan <37930113+budigunawan99@users.noreply.github.com>
2018-05-08 21:48:29 +0700

    add logout.php
    separate with server.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+ <?php
+ //Logout
+   	  session_start();
+      session_destroy();
+      unset($_SESSION['username']);
+	  unset($_SESSION['login']);
+      header('location: index.php');
+   
+
+ ?>
