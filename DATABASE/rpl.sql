b8b8d613b75c1458bc082a4a45d33f125153e921
Budi Gunawan <37930113+budigunawan99@users.noreply.github.com>
2018-05-08 14:09:38 +0700

    Newest update(8/5/18)

--- DATABASE/rpl.sql
+++ DATABASE/rpl.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2018 at 09:10 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: 08 Mei 2018 pada 09.07
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +25,124 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id_feedback` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `feedback` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `feedback`
+--
+
+INSERT INTO `feedback` (`id_feedback`, `username`, `feedback`) VALUES
+(1, 'Nanda Syafitri', 'Awesome'),
+(7, 'Rinaldi Udra Suhanda', 'Halo, terimakasih atas pelayanannya yang sangat memuaskan.'),
+(8, 'Budi Gunawan', 'Sukses Terus D\'Caffe-in'),
+(9, 'Kurniadinur', 'Menu yang tersedia sangat kekinian hehe'),
+(10, 'No name', 'Tambahin lagi menunya');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`id`, `username`, `password`, `jabatan`) VALUES
+(0, 'staff', '1253208465b1efa876f982d8a9e73eef', 'barista'),
+(1, 'staff', '1253208465b1efa876f982d8a9e73eef', 'pelayan'),
+(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'kasir'),
+(3, 'staff', '1253208465b1efa876f982d8a9e73eef', 'pelayan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `meja`
+--
+
+CREATE TABLE `meja` (
+  `id` int(11) NOT NULL,
+  `no_meja` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'tersedia',
+  `jumlah_kursi` int(11) NOT NULL DEFAULT '0',
+  `jam` time DEFAULT '00:00:00',
+  `tanggal` date DEFAULT '0000-00-00',
+  `pemesan` varchar(50) DEFAULT NULL,
+  `hp` varchar(20) NOT NULL DEFAULT '-'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `meja`
+--
+
+INSERT INTO `meja` (`id`, `no_meja`, `status`, `jumlah_kursi`, `jam`, `tanggal`, `pemesan`, `hp`) VALUES
+(1, 1, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(2, 2, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(3, 3, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(4, 4, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(5, 5, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(6, 6, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(7, 7, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(8, 8, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(9, 9, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(10, 10, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(11, 11, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(12, 12, 'tidak tersedia', 5, '12:00:00', '2018-09-09', 'Nanda Syafitri', '082211276007'),
+(13, 13, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(14, 14, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(15, 15, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(16, 16, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(17, 17, 'tersedia', 0, NULL, NULL, NULL, '-'),
+(18, 18, 'tersedia', 0, NULL, NULL, NULL, '-');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` varchar(255) NOT NULL,
+  `nama_menu` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `rating` int(11) NOT NULL DEFAULT '0',
+  `lama_pembuatan` int(11) NOT NULL DEFAULT '0',
+  `gambar` varchar(255) DEFAULT NULL,
+  `hp` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `menu`
+--
+
+INSERT INTO `menu` (`id`, `nama_menu`, `harga`, `rating`, `lama_pembuatan`, `gambar`, `hp`) VALUES
+('drink_01', 'Teh Hijau Tarek', 10000, 0, 10, NULL, NULL),
+('drink_02', 'Chocolate', 8000, 0, 10, NULL, NULL),
+('drink_03', 'Milkshake', 15000, 0, 15, NULL, NULL),
+('drink_04', 'Lemon Tea', 7000, 0, 8, NULL, NULL),
+('food_01', 'Roti Bakar Oreo', 10000, 0, 15, NULL, NULL),
+('food_02', 'Kentang Goreng', 15000, 0, 20, NULL, NULL),
+('food_03', 'Indomie Rebus', 15000, 0, 20, NULL, NULL),
+('food_04', 'Pisang Goreng', 8000, 0, 10, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -31,19 +150,46 @@ CREATE TABLE `users` (
   `nama` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `activation` varchar(300) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`) VALUES
-(1, '', 'nanda', 'nanda@example.com', '859a37720c27b9f70e11b79bab9318fe');
+INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `activation`, `status`) VALUES
+(1, '', 'nanda', 'nanda@example.com', '859a37720c27b9f70e11b79bab9318fe', '', '0'),
+(20, '', 'budi99', 'budi99@mhs.unsyiah.ac.id', '81dc9bdb52d04dc20036dbd8313ed055', 'dd3f6f2e8583eb607116c95eab7429ec', '0');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id_feedback`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -56,10 +202,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
